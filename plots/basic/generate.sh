5936bece3bdbdeb8448b72e3c8803ed0bf4ee996
Nils Asmussen <nils@script-solution.de>
2015-05-23 09:53:31 +0200

    Moved linux functions to tools and extract lines by name.

--- plots/basic/generate.sh
+++ plots/basic/generate.sh
@@ -3,61 +3,31 @@
 avgs=$1/basic-times.dat
 stddev=$1/basic-stddevs.dat
 
-lx_avg() {
-    grep "$2" $1 | cut -d ':' -f 2 | awk '{ printf "%d", $1 }'
-}
-
-lx_stddev() {
-    # tr to remove the \r
-    grep "$2" $1 | cut -d ':' -f 2 | sed -e 's/[^\(]*(\([[:digit:]]*\))/\1/' | tr -d '[[:space:]]'
-}
-
-cache_misses() {
-    # we have run it once with 13 cycles for M and once with 30 cycles.
-    # thus, the equations are:
-    # $ai = 1 * C + 13 * n
-    # $bi = 1 * C + 30 * n
-    # the right side is the matrix [1, 13; 1, 30].
-    # we solve it by dividing it by [$ai; $bi] and take the second element to get C (opposite order)
-
-    val=`octave -q --eval "A = [1, 13; 1, 30]; b = [$1; $2]; x = A \ b; round(nth_element(x, 2))" | awk '{ print $3 }'`
-    if [ $val -lt $2 ]; then
-        echo $(($2 - $val))
-    else
-        echo 0
-    fi
-}
-
-lx_times() {
-    lx30=`lx_avg $1/lx-30cycles.txt "$2"`
-    lx13=`lx_avg $1/lx-13cycles.txt "$2"`
-    cm=`cache_misses $lx13 $lx30`
-    echo $(($lx30 - $cm)) $cm
-}
+source tools/linux.sh
 
 # -- averages --
 echo "M3 Linux LinuxCM" > $avgs
 
 # syscall
 echo -n "`./tools/m3-avg.awk < $1/m3-syscall.txt`" >> $avgs
-echo `lx_times $1 "Time per syscall"` >> $avgs
+echo `lx_times $1 IDX_SYSCALL` >> $avgs
 
 # pthread
 m3run=`./tools/m3-avg.awk < $1/m3-vpes.txt | awk '{ print $1 + $2 }'`
-echo $m3run `lx_times $1 "Cycles per pthread_start"` >> $avgs
+echo $m3run `lx_times $1 IDX_PTHREAD` >> $avgs
 
 # clone
-echo $m3run `lx_times $1 "Cycles per clone"` >> $avgs
+echo $m3run `lx_times $1 IDX_CLONE` >> $avgs
 
 # fork
-echo $m3run `lx_times $1 "Cycles per fork "` >> $avgs
+echo $m3run `lx_times $1 IDX_FORK` >> $avgs
 
 # exec
 m3exec=`./tools/m3-avg.awk < $1/m3-vpes.txt | awk '{ print $1 + $4 }'`
-echo $m3exec `lx_times $1 "Cycles per fork+exec"` >> $avgs
+echo $m3exec `lx_times $1 IDX_EXEC` >> $avgs
 
 # vfork
-echo $m3exec `lx_times $1 "Cycles per vfork+exec"` >> $avgs
+echo $m3exec `lx_times $1 IDX_VEXEC` >> $avgs
 echo >> $avgs
 
 
@@ -70,18 +40,18 @@ echo "`./tools/m3-stddev.awk < $1/m3-syscall.txt | tr -d '[[:space:]]'`" \
     "`grep 0003 $1/m3-vpes.txt | ./tools/m3-stddev.awk | tr -d '[[:space:]]'`" >> $stddev
 
 # row 2 (lx syscall, lx pthread, lx exec)
-echo "`lx_stddev $1/lx-30cycles.txt 'Time per syscall'`" \
-    "`lx_stddev $1/lx-30cycles.txt 'Cycles per pthread_start'`" \
-    "`lx_stddev $1/lx-30cycles.txt 'Cycles per fork+exec'`" >> $stddev
+echo "`lx_stddev $1/lx-30cycles.txt IDX_SYSCALL`" \
+    "`lx_stddev $1/lx-30cycles.txt IDX_PTHREAD`" \
+    "`lx_stddev $1/lx-30cycles.txt IDX_EXEC`" >> $stddev
 
 # row 3 (0, lx clone, lx vfork)
 echo 0 \
-    `lx_stddev $1/lx-30cycles.txt "Cycles per clone"` \
-    `lx_stddev $1/lx-30cycles.txt "Cycles per vfork+exec"` >> $stddev
+    `lx_stddev $1/lx-30cycles.txt IDX_CLONE` \
+    `lx_stddev $1/lx-30cycles.txt IDX_VEXEC` >> $stddev
 
 # row 4 (0, lx fork, 0)
 echo 0 \
-    `lx_stddev $1/lx-30cycles.txt "Cycles per fork "` \
+    `lx_stddev $1/lx-30cycles.txt IDX_FORK` \
     0 >> $stddev
 echo >> $stddev
 
---
+++ tools/linux.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+get_line() {
+    php -- $1 $2 <<'EOF'
+<?php
+$i = 0;
+$indices = array();
+$indices['IDX_SYSCALL'] =             $i++;
+$indices['IDX_TASKCRT'] =             $i++;
+$indices['IDX_PTHREAD'] =             $i++;
+$indices['IDX_CLONE'] =               $i++;
+$indices['IDX_FORK'] =                $i++;
+$indices['IDX_EXEC'] =                $i++;
+$indices['IDX_VEXEC'] =               $i++;
+$indices['IDX_READ'] =                $i++;
+$indices['IDX_READ_MEMCPY'] =         $i++;
+$indices['IDX_READ_MMAP'] =           $i++;
+$indices['IDX_READ_MMAP_AGAIN'] =     $i++;
+$indices['IDX_READ_MMAP_CP'] =        $i++;
+$indices['IDX_READ_MMAP_CP_AGAIN'] =  $i++;
+$indices['IDX_CHKSUM_READ'] =         $i++;
+$indices['IDX_CHKSUM_READ_MEMCPY'] =  $i++;
+$indices['IDX_CHKSUM_MMAP'] =         $i++;
+$indices['IDX_CHKSUM_MMAP_AGAIN'] =   $i++;
+$indices['IDX_WRITE'] =               $i++;
+$indices['IDX_WRITE_MEMCPY'] =        $i++;
+$indices['IDX_COPY_RDWR'] =           $i++;
+$indices['IDX_COPY_RDWR_MEMCPY'] =    $i++;
+$indices['IDX_COPY_MMAP'] =           $i++;
+$indices['IDX_COPY_MMAP_AGAIN'] =     $i++;
+$indices['IDX_PIPE'] =                $i++;
+$indices['IDX_PIPE_MEMCPY'] =         $i++;
+
+$lines = file($argv[1]);
+echo $lines[$indices[$argv[2]]];
+?>
+EOF
+}
+
+cache_misses() {
+    # we have run it once with 13 cycles for M and once with 30 cycles.
+    # thus, the equations are:
+    # $ai = 1 * C + 13 * n
+    # $bi = 1 * C + 30 * n
+    # the right side is the matrix [1, 13; 1, 30].
+    # we solve it by dividing it by [$ai; $bi] and take the second element to get C (opposite order)
+
+    val=`octave -q --eval "A = [1, 13; 1, 30]; b = [$1; $2]; x = A \ b; round(nth_element(x, 2))" | awk '{ print $3 }'`
+    if [ $val -lt $2 ]; then
+        echo $(($2 - $val))
+    else
+        echo 0
+    fi
+}
+
+lx_avg() {
+    get_line $1 $2 | cut -d ':' -f 2 | awk '{ printf "%d", $1 }'
+}
+
+lx_stddev() {
+    # tr to remove the \r
+    get_line $1 $2 | cut -d ':' -f 2 | sed -e 's/[^\(]*(\([[:digit:]]*\))/\1/' | tr -d '[[:space:]]'
+}
+
+lx_times() {
+    lx30=`lx_avg $1/lx-30cycles.txt "$2"`
+    lx13=`lx_avg $1/lx-13cycles.txt "$2"`
+    cm=`cache_misses $lx13 $lx30`
+    echo $(($lx30 - $cm)) $cm
+}
