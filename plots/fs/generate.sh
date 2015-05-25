bb58b26d3e6686dcd29d59a739a865957a97eda3
Nils Asmussen <nils@script-solution.de>
2015-05-25 12:06:45 +0200

    Better show pipe-bench in fs plot, not checksum.

--- plots/fs/generate.sh
+++ plots/fs/generate.sh
@@ -2,19 +2,19 @@
 
 read_avgs=$1/fs-read-times.dat
 read_stddev=$1/fs-read-stddevs.dat
-chksum_avgs=$1/fs-chksum-times.dat
-chksum_stddev=$1/fs-chksum-stddevs.dat
 write_avgs=$1/fs-write-times.dat
 write_stddev=$1/fs-write-stddevs.dat
 copy_avgs=$1/fs-copy-times.dat
 copy_stddev=$1/fs-copy-stddevs.dat
+pipe_avgs=$1/fs-pipe-times.dat
+pipe_stddev=$1/fs-pipe-stddevs.dat
 
 source tools/linux.sh
 
 echo "M3 Lx Lxnc" > $read_avgs
-echo "M3 Lx Lxnc" > $chksum_avgs
 echo "M3 Lx Lxnc" > $write_avgs
 echo "M3 Lx Lxnc" > $copy_avgs
+echo "M3 Lx Lxnc" > $pipe_avgs
 
 m3trans=$((2048 * 1024 / 8))
 
@@ -22,11 +22,6 @@ m3read=350000
 echo $((m3read - $m3trans)) `lx_rem_time $1 IDX_READ IDX_READ_MEMCPY` >> $read_avgs
 echo $m3trans `lx_copy_time $1 IDX_READ IDX_READ_MEMCPY` >> $read_avgs
 echo 0 0 0 >> $read_avgs
-
-m3chksum=3505641
-echo $((m3chksum - $m3trans)) `lx_copy_time $1 IDX_CHKSUM_MMAP IDX_CHKSUM_MMAP_AGAIN` >> $chksum_avgs
-echo $m3trans 0 0 >> $chksum_avgs
-echo 0 `lx_rem_time $1 IDX_CHKSUM_MMAP IDX_CHKSUM_MMAP_AGAIN` >> $chksum_avgs
 
 m3write=550834
 echo $((m3write - $m3trans)) `lx_rem_time $1 IDX_WRITE IDX_WRITE_MEMCPY` >> $write_avgs
@@ -38,18 +33,23 @@ echo $((m3copy - $m3trans * 2)) 0 0 >> $copy_avgs
 echo $(($m3trans * 2)) `lx_copy_time $1 IDX_COPY_MMAP IDX_COPY_MMAP_AGAIN` >> $copy_avgs
 echo 0 `lx_rem_time $1 IDX_COPY_MMAP IDX_COPY_MMAP_AGAIN` >> $copy_avgs
 
+m3pipe=1000000
+echo $((m3pipe - $m3trans * 2)) `lx_rem_time $1 IDX_PIPE IDX_PIPE_MEMCPY` >> $pipe_avgs
+echo $(($m3trans * 2)) `lx_copy_time $1 IDX_PIPE IDX_PIPE_MEMCPY` >> $pipe_avgs
+echo 0 0 0 >> $pipe_avgs
+
 echo "" > $read_stddev
-echo "" > $chksum_stddev
 echo "" > $write_stddev
 echo "" > $copy_stddev
+echo "" > $pipe_stddev
 
 cat $read_avgs
-cat $chksum_avgs
+cat $pipe_avgs
 cat $write_avgs
 cat $copy_avgs
 
 Rscript plots/fs/plot.R $1/fs.pdf \
     $read_avgs $read_stddev \
-    $chksum_avgs $chksum_stddev \
     $write_avgs $write_stddev \
-    $copy_avgs $copy_stddev
+    $copy_avgs $copy_stddev \
+    $pipe_avgs $pipe_stddev
--- plots/fs/plot.R
+++ plots/fs/plot.R
@@ -3,16 +3,16 @@ scaling <- 1.3
 namescale <- 1.15
 
 rdtimes <- read.table(as.character(args[2]), header=TRUE, sep=" ")
-cktimes <- read.table(as.character(args[4]), header=TRUE, sep=" ")
-wrtimes <- read.table(as.character(args[6]), header=TRUE, sep=" ")
-cptimes <- read.table(as.character(args[8]), header=TRUE, sep=" ")
+wrtimes <- read.table(as.character(args[4]), header=TRUE, sep=" ")
+cptimes <- read.table(as.character(args[6]), header=TRUE, sep=" ")
+pitimes <- read.table(as.character(args[8]), header=TRUE, sep=" ")
 
 pdf(as.character(args[1]), width=7, height=5)
 
 layout(matrix(c(1,2,3,4), 1, 4, byrow = TRUE),
     widths=c(1.4,1,1,1), heights=c(1,1))
 
-par(mar=c(5,5,3,2))
+par(mar=c(6,5,2,2))
 
 barplot(as.matrix(rdtimes), beside=F,
     ylim=c(0,14000000), space=c(0.3, 0, 0), ylab="Time (cycles)",
@@ -22,7 +22,7 @@ box(col = 'black')
 
 legend("topleft", c("Remaining", "Data transfers", "Pagefaults"), cex=1, fill=gray.colors(3))
 
-par(mar=c(5,0,3,2))
+par(mar=c(6,0,2,2))
 
 barplot(as.matrix(wrtimes), beside=F,
     ylim=c(0,14000000), space=c(0.3, 0, 0), axes=F,
@@ -30,15 +30,7 @@ barplot(as.matrix(wrtimes), beside=F,
     names.arg=c("M3","Lx","Lx-$"), sub="Write")
 box(col = 'black')
 
-par(mar=c(5,0,3,2))
-
-barplot(as.matrix(cktimes), beside=F,
-    ylim=c(0,14000000), space=c(0.3, 0, 0), axes=F,
-    cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling, cex.names=namescale,
-    names.arg=c("M3","Lx","Lx-$"), sub="Checksum")
-box(col = 'black')
-
-par(mar=c(5,0,3,2))
+par(mar=c(6,0,2,2))
 
 barplot(as.matrix(cptimes), beside=F,
     ylim=c(0,14000000), space=c(0.3, 0, 0), axes=F,
@@ -46,6 +38,14 @@ barplot(as.matrix(cptimes), beside=F,
     names.arg=c("M3","Lx","Lx-$"), sub="Copy")
 box(col = 'black')
 
-par(mar=c(5,0,3,2))
+par(mar=c(6,0,2,2))
+
+barplot(as.matrix(pitimes), beside=F,
+    ylim=c(0,14000000), space=c(0.3, 0, 0), axes=F,
+    cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling, cex.names=namescale,
+    names.arg=c("M3","Lx","Lx-$"), sub="Pipe")
+box(col = 'black')
+
+par(mar=c(6,0,2,2))
 
 dev.off()
