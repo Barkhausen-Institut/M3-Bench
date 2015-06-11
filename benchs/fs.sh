e4a76c6bd3553aa23620801634387dba2951bbdd
Nils Asmussen <nils@script-solution.de>
2015-06-11 22:40:48 +0200

    Use bench.img in fs bench

--- benchs/fs.sh
+++ benchs/fs.sh
@@ -4,7 +4,7 @@ rdcfg=`readlink -f benchs/filereader.cfg`
 wrcfg=`readlink -f benchs/filewriter.cfg`
 
 cd m3/XTSC
-export M3_TARGET=t3 M3_BUILD=bench
+export M3_TARGET=t3 M3_BUILD=bench M3_FS=bench.img
 
 ./b run $rdcfg
 ./tools/bench.sh xtsc.log > $1/m3-fsread.txt
