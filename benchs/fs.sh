867f27b5235630b6c51ff88cddf8bae7e0e16901
Nils Asmussen <nils@script-solution.de>
2015-06-11 14:08:33 -0700

    Pipe benchmark: don't let reader and writer run in parallel.

--- benchs/fs.sh
+++ benchs/fs.sh
@@ -16,5 +16,13 @@ export M3_TARGET=t3 M3_BUILD=bench M3_FS=bench.img
 ./b run $cpcfg
 ./tools/bench.sh xtsc.log > $1/m3-fscopy.txt
 
-./b run boot/bench-pipe.cfg
+# ensure that we don't let reader and writer run in parallel
+sed --in-place -e 's/#if defined(__t2__)/#if defined(__t2__) || defined(__t3__)/' include/m3/pipe/Pipe.h
+
+# build everything and undo the changes
+./b
+
+sed --in-place -e 's/#if defined(__t2__) || defined(__t3__)/#if defined(__t2__)/' include/m3/pipe/Pipe.h
+
+./b run boot/bench-pipe.cfg -n
 ./tools/bench.sh xtsc.log > $1/m3-pipe.txt
