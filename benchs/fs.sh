f7b9dbd3aa9e615808a58689046ea892552ef726
Nils Asmussen <nils@script-solution.de>
2015-06-11 12:57:55 -0700

    Fix.

--- benchs/fs.sh
+++ benchs/fs.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-
-rdcfg=`readlink -f benchs/fs/filereader.cfg`
-wrcfg=`readlink -f benchs/fs/filewriter.cfg`
+rdcfg=`readlink -f benchs/filereader.cfg`
+wrcfg=`readlink -f benchs/filewriter.cfg`
 
 cd m3/XTSC
 export M3_TARGET=t3 M3_BUILD=bench
