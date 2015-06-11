cebac30f674fd103807bdc158c0900d5bb5a3e72
Nils Asmussen <nils@script-solution.de>
2015-06-11 12:55:30 -0700

    Improved fs microbench.

--- benchs/fs.sh
+++ benchs/fs.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+
+rdcfg=`readlink -f benchs/fs/filereader.cfg`
+wrcfg=`readlink -f benchs/fs/filewriter.cfg`
+
 cd m3/XTSC
 export M3_TARGET=t3 M3_BUILD=bench
 
-./b run ../../filereader.cfg
+./b run $rdcfg
 ./tools/bench.sh xtsc.log > $1/m3-fsread.txt
 
-./b run boot/filewriter.cfg
+./b run $wrcfg
 ./tools/bench.sh xtsc.log > $1/m3-fswrite.txt
