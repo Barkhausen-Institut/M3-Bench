b6ca610bbec4a17bc54f164fe28bdf94c9448011
Nils Asmussen <nils@script-solution.de>
2015-06-19 16:40:33 +0200

    fstrace: more or less complete now.

--- benchs/fstrace.sh
+++ benchs/fstrace.sh
@@ -1,39 +1,22 @@
 #!/bin/bash
 
-run_bench() {
-    cd xtensa-linux
+gen_timedtrace() {
+    grep -B10000 "===" $1 | grep -v "===" > $1-strace
+    grep -A10000 "===" $1 | grep -v "===" > $1-timings
 
-    LX_THCMP=1 ./b fsbench > $1/lx-fstrace-$2-30cycles.txt
+    # for untar: prefix relative paths with /tardata/
+    sed --in-place -e 's/("\([^/]\)/("\/tardata\/\1/g' $1-strace
 
-    cd -
+    ./tools/timedstrace.php trace $1-strace $1-timings > $1-timedstrace
 
-    gen_timedtrace() {
-        grep -B10000 "===" $1 | grep -v "===" > $1-strace
-        grep -A10000 "===" $1 | grep -v "===" > $1-timings
-        ./tools/timedstrace.php trace $1-strace $1-timings > $1-timedstrace
-    }
+    # make the strace a little more friendly for strace2cpp
+    sed --in-place -e 's/\/\* \([[:digit:]]*\) entries \*\//\1/' $1-timedstrace
+    sed --in-place -e 's/\/\* d_reclen == 0, problem here \*\///' $1-timedstrace
 
-    gen_timedtrace $1/lx-fstrace-$2-30cycles.txt
+    awk '{ print $1, $2, $4 - $3 }' $1-timings > $1-timings-human
+}
 
-    awk '{ print $1, $2, $4 - $3 }' $1/lx-fstrace-$2-30cycles.txt-timings \
-       > $1/lx-fstrace-$2-30cycles.txt-timings-human
-
-    cd m3/XTSC
-    export M3_TARGET=t3 M3_BUILD=bench M3_FS=bench.img
-
-    ./b
-
-    # first, make the strace a little more friendly for strace2cpp
-    sed --in-place -e 's/\/\* \([[:digit:]]*\) entries \*\//\1/' $1/lx-fstrace-$2-30cycles.txt-timedstrace
-    sed --in-place -e 's/\/\* d_reclen == 0, problem here \*\///' $1/lx-fstrace-$2-30cycles.txt-timedstrace
-
-    ./build/t3-sim-bench/apps/fstrace/strace2cpp/strace2cpp \
-        < $1/lx-fstrace-$2-30cycles.txt-timedstrace > $1/lx-fstrace-$2-30cycles.txt-opcodes.c
-    cp $1/lx-fstrace-$2-30cycles.txt-opcodes.c apps/fstrace/m3fs/trace.c
-
-    ./b run boot/fstrace.cfg
-    ./tools/bench.sh xtsc.log > $1/m3-fstrace.$2-txt
-
+extract_result() {
     awk 'BEGIN {
         start = 0
     }
@@ -55,12 +38,44 @@ run_bench() {
     /^[[:space:]]*\[[[:space:]]*[[:digit:]]+\][[:space:]]*/ {
         end = $3
     }
-    ' $1/lx-fstrace-$2-30cycles.txt-timings > $1/lx-fstrace-$2-result.txt
+    ' $1
+}
+
+wait_time() {
+    echo -n "Wait: " >> $2
+    ./tools/timedstrace.php waittime $1-strace $1-timings >> $2
+}
+
+run_bench() {
+    cd xtensa-linux
+
+    ./b fsbench > $1/lx-fstrace-$2-13cycles.txt
+    LX_THCMP=1 ./b fsbench > $1/lx-fstrace-$2-30cycles.txt
 
     cd -
-    echo -n "Wait: " >> $1/lx-fstrace-$2-result.txt
-    ./tools/timedstrace.php waittime $1/lx-fstrace-$2-30cycles.txt-strace \
-        $1/lx-fstrace-$2-30cycles.txt-timings >> $1/lx-fstrace-$2-result.txt
+
+    gen_timedtrace $1/lx-fstrace-$2-13cycles.txt
+    gen_timedtrace $1/lx-fstrace-$2-30cycles.txt
+
+    cd m3/XTSC
+    export M3_TARGET=t3 M3_BUILD=bench M3_FS=bench.img
+
+    ./b
+
+    ./build/t3-sim-bench/apps/fstrace/strace2cpp/strace2cpp \
+        < $1/lx-fstrace-$2-30cycles.txt-timedstrace > $1/lx-fstrace-$2-30cycles.txt-opcodes.c
+    cp $1/lx-fstrace-$2-30cycles.txt-opcodes.c apps/fstrace/m3fs/trace.c
+
+    ./b run boot/fstrace.cfg
+    ./tools/bench.sh xtsc.log > $1/m3-fstrace.$2-txt
+
+    extract_result $1/lx-fstrace-$2-13cycles.txt-timings > $1/lx-fstrace-$2-13cycles-result.txt
+    extract_result $1/lx-fstrace-$2-30cycles.txt-timings > $1/lx-fstrace-$2-30cycles-result.txt
+
+    cd -
+
+    wait_time $1/lx-fstrace-$2-13cycles.txt $1/lx-fstrace-$2-13cycles-result.txt
+    wait_time $1/lx-fstrace-$2-30cycles.txt $1/lx-fstrace-$2-30cycles-result.txt
 }
 
 cd xtensa-linux
@@ -74,3 +89,5 @@ cd -
 FSBENCH_CMD="find /default -name test" run_bench $1 find
 
 FSBENCH_CMD="tar -cf /tmp/test.tar /tardata" run_bench $1 tar
+
+FSBENCH_CMD="tar -xf /test.tar -C /tardata" run_bench $1 untar
