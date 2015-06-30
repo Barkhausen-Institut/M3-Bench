458a7f86fe8c83276ed8fec0b974d87983d651e4
Nils Asmussen <nils@script-solution.de>
2015-06-30 15:14:11 +0200

    Added sqlite to applevel benchmarks.

--- plots/applevel/generate.sh
+++ plots/applevel/generate.sh
@@ -8,6 +8,7 @@ pipetr_avgs=$1/applevel-pipetr-times.dat
 tar_avgs=$1/applevel-tar-times.dat
 untar_avgs=$1/applevel-untar-times.dat
 find_avgs=$1/applevel-find-times.dat
+sqlite_avgs=$1/applevel-sqlite-times.dat
 
 source tools/linux.sh
 
@@ -15,11 +16,13 @@ echo "M3 Lx Lxnc" > $pipetr_avgs
 echo "M3 Lx Lxnc" > $tar_avgs
 echo "M3 Lx Lxnc" > $untar_avgs
 echo "M3 Lx Lxnc" > $find_avgs
+echo "M3 Lx Lxnc" > $sqlite_avgs
 
 # size of the file we read
 m3pipetrtrans=$((4 * 64 * 1024 / 8))
 # size of the tar file we create/extract; 2* because we need to read that amount and write it, too
 m3tartrans=$((2 * 1216000 / 8))
+m3sqlitetrans=$((57704 / 8))
 
 m3pipetr=`grep 0000 $1/m3-pipetr-1.cfg-result.txt | ./tools/m3-avg.awk`
 echo $(($m3pipetr - $m3pipetrtrans)) `lx_rem_time $1/lx IDX_PIPETR IDX_PIPETR_MEMCPY` >> $pipetr_avgs
@@ -44,8 +47,15 @@ echo $(($m3find - $m3wait)) `lx_fstrace_total $1/lx-fstrace-find-result` >> $fin
 echo 0 `lx_copy_time $1/lx-fstrace-find-result IDX_FSTRACE_MEMCPY` >> $find_avgs
 echo $m3wait `lx_copy_time $1/lx-fstrace-find-result IDX_FSTRACE_WAIT` >> $find_avgs
 
+m3sqlite=`grep 0000 $1/m3-fstrace.sqlite-txt | ./tools/m3-avg.awk`
+m3wait=`get_m3_waittime $1/m3-fstrace.sqlite-txt`
+echo $(($m3sqlite - $m3sqlitetrans - $m3wait)) `lx_fstrace_total $1/lx-fstrace-sqlite-result` >> $sqlite_avgs
+echo $(($m3sqlitetrans)) `lx_copy_time $1/lx-fstrace-sqlite-result IDX_FSTRACE_MEMCPY` >> $sqlite_avgs
+echo $m3wait `lx_copy_time $1/lx-fstrace-sqlite-result IDX_FSTRACE_WAIT` >> $sqlite_avgs
+
 Rscript plots/applevel/plot.R $1/applevel.pdf \
     $pipetr_avgs \
     $tar_avgs \
     $untar_avgs \
-    $find_avgs
+    $find_avgs \
+    $sqlite_avgs
--- plots/applevel/plot.R
+++ plots/applevel/plot.R
@@ -6,11 +6,12 @@ rdtimes <- read.table(as.character(args[2]), header=TRUE, sep=" ") / 1000000
 wrtimes <- read.table(as.character(args[3]), header=TRUE, sep=" ") / 1000000
 cptimes <- read.table(as.character(args[4]), header=TRUE, sep=" ") / 1000000
 pitimes <- read.table(as.character(args[5]), header=TRUE, sep=" ") / 1000000
+sqtimes <- read.table(as.character(args[6]), header=TRUE, sep=" ") / 1000000
 
 pdf(as.character(args[1]), width=7, height=5)
 
-layout(matrix(c(1,2,3,4), 1, 4, byrow = TRUE),
-    widths=c(1.4,1,1,1), heights=c(1,1))
+layout(matrix(c(1,2,3,4,5), 1, 5, byrow = TRUE),
+    widths=c(1.55,1,1,1,1), heights=c(1,1))
 
 par(mar=c(6,5,2,2))
 
@@ -19,6 +20,8 @@ barplot(as.matrix(rdtimes), beside=F,
     cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling, cex.names=namescale,
     names.arg=c("M3","Lx","Lx-$"), sub="cat | tr")
 box(col = 'black')
+
+legend("topleft", c("Wait", "Data xfers", "Remaining"), cex=1, fill=rev(gray.colors(3)))
 
 par(mar=c(6,0,2,2))
 
@@ -44,7 +47,13 @@ barplot(as.matrix(pitimes), beside=F,
     names.arg=c("M3","Lx","Lx-$"), sub="find")
 box(col = 'black')
 
-legend("topright", c("Wait", "Data transfers", "Remaining"), cex=1, fill=rev(gray.colors(3)))
+par(mar=c(6,0,2,2))
+
+barplot(as.matrix(sqtimes), beside=F,
+    ylim=c(0,6), space=c(0.3, 0, 0), axes=F,
+    cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling, cex.names=namescale,
+    names.arg=c("M3","Lx","Lx-$"), sub="sqlite")
+box(col = 'black')
 
 par(mar=c(6,0,2,2))
 
