e297492e967412c6fdc4a2ed8e20b94ec6e0e2c3
Nils Asmussen <nils@script-solution.de>
2015-07-15 12:01:06 +0200

    Added figure for syscalls and fs benchmarks.

---
+++ plots/sys_fs/generate.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+sctimes=$1/sys_fs-sysc-times.dat
+scstddev=$1/sys_fs-sysc-stddev.dat
+
+read_avgs=$1/sys_fs-read-times.dat
+read_stddev=$1/sys_fs-read-stddevs.dat
+write_avgs=$1/sys_fs-write-times.dat
+write_stddev=$1/sys_fs-write-stddevs.dat
+pipe_avgs=$1/sys_fs-pipe-times.dat
+pipe_stddev=$1/sys_fs-pipe-stddevs.dat
+
+source tools/linux.sh
+
+echo "M3 Lx Lxnc" > $sctimes
+echo "M3 Lx Lxnc" > $read_avgs
+echo "M3 Lx Lxnc" > $write_avgs
+echo "M3 Lx Lxnc" > $pipe_avgs
+
+# syscall
+echo "`./tools/m3-avg.awk < $1/m3-syscall.txt | tr -d '[[:space:]]'`" \
+    "`lx_avg $1/lx-30cycles.txt IDX_SYSCALL`" \
+    "`lx_base_time $1/lx IDX_SYSCALL`" >> $sctimes
+echo 0 0 0 >> $sctimes
+
+echo "`./tools/m3-stddev.awk < $1/m3-syscall.txt | tr -d '[[:space:]]'`" \
+    "`lx_stddev $1/lx-30cycles.txt IDX_SYSCALL`" \
+    "`lx_stddev $1/lx-13cycles.txt IDX_SYSCALL`" > $scstddev
+
+# fs
+m3trans=$((2048 * 1024 / 8))
+
+m3read=`grep 0001 $1/m3-fsread.txt | ./tools/m3-avg.awk`
+echo $((m3read - $m3trans)) `lx_rem_time $1/lx IDX_READ IDX_READ_MEMCPY` >> $read_avgs
+echo $m3trans `lx_copy_time $1/lx IDX_READ_MEMCPY` >> $read_avgs
+
+m3write=`grep 0001 $1/m3-fswrite.txt | ./tools/m3-avg.awk`
+echo $((m3write - $m3trans)) `lx_rem_time $1/lx IDX_WRITE IDX_WRITE_MEMCPY` >> $write_avgs
+echo $m3trans `lx_copy_time $1/lx IDX_WRITE_MEMCPY` >> $write_avgs
+
+m3pipe=`grep 0000 $1/m3-pipe.txt | ./tools/m3-avg.awk`
+echo $((m3pipe - $m3trans * 2)) `lx_rem_time $1/lx IDX_PIPE IDX_PIPE_MEMCPY` >> $pipe_avgs
+echo $(($m3trans * 2)) `lx_copy_time $1/lx IDX_PIPE_MEMCPY` >> $pipe_avgs
+
+echo 0 \
+    "`lx_stddev $1/lx-30cycles.txt IDX_READ`" \
+    "`lx_stddev $1/lx-13cycles.txt IDX_READ`" > $read_stddev
+
+echo 0 \
+    "`lx_stddev $1/lx-30cycles.txt IDX_WRITE`" \
+    "`lx_stddev $1/lx-13cycles.txt IDX_WRITE`" > $write_stddev
+
+echo 0 \
+    "`lx_stddev $1/lx-30cycles.txt IDX_PIPE`" \
+    "`lx_stddev $1/lx-13cycles.txt IDX_PIPE`" > $pipe_stddev
+
+Rscript plots/sys_fs/plot.R $1/sys_fs.pdf \
+    $sctimes $scstddev \
+    $read_avgs $read_stddev \
+    $write_avgs $write_stddev \
+    $pipe_avgs $pipe_stddev
---
+++ plots/sys_fs/plot.R
@@ -0,0 +1,76 @@
+error.bar <- function(mp, means, stddevs) {
+    stDevs <- matrix(stddevs, length(stddevs))
+    # Plot the vertical lines of the error bars
+    # The vertical bars are plotted at the midpoints
+    segments(mp, means - stDevs, mp, means + stDevs, lwd=1)
+    # Now plot the horizontal bounds for the error bars
+    # 1. The lower bar
+    segments(mp - 0.1, means - stDevs, mp + 0.1, means - stDevs, lwd=1)
+    # 2. The upper bar
+    segments(mp - 0.1, means + stDevs, mp + 0.1, means + stDevs, lwd=1)
+}
+
+args <- commandArgs(trailingOnly = TRUE)
+scaling <- 1.3
+namescale <- 1.15
+
+sctimes <- read.table(as.character(args[2]), header=TRUE, sep=" ")
+scstddevs <- read.table(as.character(args[3]), header=FALSE, sep=" ")
+
+rdtimes <- read.table(as.character(args[4]), header=TRUE, sep=" ")    / 1000000
+rdstddev <- read.table(as.character(args[5]), header=FALSE, sep=" ")  / 1000000
+
+wrtimes <- read.table(as.character(args[6]), header=TRUE, sep=" ")    / 1000000
+wrstddev <- read.table(as.character(args[7]), header=FALSE, sep=" ")  / 1000000
+
+pitimes <- read.table(as.character(args[8]), header=TRUE, sep=" ")    / 1000000
+pistddev <- read.table(as.character(args[9]), header=FALSE, sep=" ")  / 1000000
+
+pdf(as.character(args[1]), width=7, height=3.5)
+par(cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling)
+
+layout(matrix(c(1,2,3,4), 1, 4, byrow = TRUE),
+    widths=c(1.5,1.4,1,1), heights=c(1,1))
+
+par(mar=c(6,5,2,2))
+
+barx <- barplot(as.matrix(sctimes), beside=F,
+    ylim=c(0.0,500), space=0, axes=T, ylab="Time (cycles)",
+    cex.names=namescale,
+    names.arg=c("M3","Lx","Lx-$"), sub="Syscall")
+
+error.bar(barx, colSums(sctimes), as.double(scstddevs))
+box(col = 'black')
+
+par(mar=c(6,4,2,2))
+
+barx <- barplot(as.matrix(rdtimes), beside=F,
+    ylim=c(0,9), space=c(0.3, 0, 0), axes=T, ylab="Time (M cycles)",
+    cex.names=namescale,
+    names.arg=c("M3","Lx","Lx-$"), sub="Read")
+error.bar(barx, colSums(rdtimes), as.double(rdstddev))
+box(col = 'black')
+
+par(mar=c(6,0,2,2))
+
+barx <- barplot(as.matrix(wrtimes), beside=F,
+    ylim=c(0,9), space=c(0.3, 0, 0), axes=F,
+    cex.names=namescale,
+    names.arg=c("M3","Lx","Lx-$"), sub="Write")
+error.bar(barx, colSums(wrtimes), as.double(wrstddev))
+box(col = 'black')
+
+par(mar=c(6,0,2,2))
+
+barx <- barplot(as.matrix(pitimes), beside=F,
+    ylim=c(0,9), space=c(0.3, 0, 0), axes=F,
+    cex.names=namescale,
+    names.arg=c("M3","Lx","Lx-$"), sub="Pipe")
+error.bar(barx, colSums(pitimes), as.double(pistddev))
+box(col = 'black')
+
+legend("topright", c("Data transfers", "Remaining"), cex=1, fill=rev(gray.colors(2)))
+
+par(mar=c(6,0,2,2))
+
+dev.off()
