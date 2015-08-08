549511564c06a9030ac2459e0f33bd9529e95cbb
Nils Asmussen <nils@script-solution.de>
2015-08-08 17:17:01 +0200

    Improved fft figure.

--- plots/fft/plot.R
+++ plots/fft/plot.R
@@ -4,13 +4,13 @@ namescale <- 1.15
 
 osname <- as.character(args[2])
 
-times <- read.table(as.character(args[3]), header=TRUE, sep=" ") / 1000
+times <- read.table(as.character(args[3]), header=TRUE, sep=" ") / 1000000
 
 pdf(as.character(args[1]), width=7, height=3.5)
 par(cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling)
 
 barplot(as.matrix(times), beside=F,
-    ylim=c(0,3000), space=c(0.3, 0, 0), ylab="Time (K cycles)",
+    ylim=c(0,3), space=c(0.3, 0, 0), ylab="Time (M cycles)",
     cex.names=namescale,
     names.arg=c("Linux",osname,paste(osname,"accelerator",sep="+")))
 box(col = 'black')
