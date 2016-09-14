f809540cb382ede6a1ccee7d0d9954dd40495992
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-09-14 15:38:02 +0200

    Updated pipe benchmarks.

--- benchs/fs.sh
+++ benchs/fs.sh
@@ -18,16 +18,11 @@ export M3_BUILD=bench M3_FS=bench.img
 ./b run $cpcfg
 ./src/tools/bench.sh $M3_LOG > $1/m3-fscopy.txt
 
-# ensure that we don't let reader and writer run in parallel
-sed --in-place -e 's/#if defined(__t2__)/#if defined(__t2__) || defined(__t3__)/' src/include/m3/pipe/DirectPipe.h
-
-# build bench-pipe and undo the changes
-scons build/$M3_TARGET-$M3_BUILD/bin/bench-pipe
-
-sed --in-place -e 's/#if defined(__t2__) || defined(__t3__)/#if defined(__t2__)/' src/include/m3/pipe/DirectPipe.h
+# rebuild bench-pipe so that reader/writer are forced to take turns
+M3_CFLAGS="-DSINGLE_ITEM_BUF=1" scons build/$M3_TARGET-$M3_BUILD/bin/bench-pipe
 
 ./b run $pipedir -n
 ./src/tools/bench.sh $M3_LOG > $1/m3-pipe-direct.txt
 
-./b run $pipeindir
+./b run $pipeindir -n
 ./src/tools/bench.sh $M3_LOG > $1/m3-pipe-indirect.txt
