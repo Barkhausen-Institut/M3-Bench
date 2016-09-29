9f6f91317d0e76bbc8fa39737d53861761d92687
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-09-29 17:10:03 +0200

    rctmux.sh: split into two functions.

--- benchs/rctmux.sh
+++ benchs/rctmux.sh
@@ -9,20 +9,27 @@ export M3_GEM5_CFG=config/caches.py
 export M3_GEM5_DBG=Dtu,DtuRegWrite,DtuCmd,DtuConnector
 # export M3_GEM5_CPU=timing
 
-run_fstrace() {
+run_fstrace_alone() {
     /bin/echo -e "\e[1mRunning $2-alone...\e[0m"
     cp $1/lx-fstrace-$2-30cycles.txt-opcodes.c src/apps/fstrace/m3fs/trace.c
     M3_CORES=6 M3_RCTMUX_ARGS="0 2 /bin/fstrace-m3fs /tmp/1/ /bin/fstrace-m3fs /tmp/2/" ./b run $cfg
     ./src/tools/bench.sh $M3_LOG > $1/m3-rctmux-$2-alone.txt
     cp $M3_LOG $1/m3-rctmux-$2-alone.log
+}
 
+run_fstrace_shared() {
     /bin/echo -e "\e[1mRunning $2-shared...\e[0m"
+    cp $1/lx-fstrace-$2-30cycles.txt-opcodes.c src/apps/fstrace/m3fs/trace.c
     M3_CORES=5 M3_RCTMUX_ARGS="1 2 /bin/fstrace-m3fs /tmp/1/ /bin/fstrace-m3fs /tmp/2/" ./b run $cfg
     ./src/tools/bench.sh $M3_LOG > $1/m3-rctmux-$2-shared.txt
     cp $M3_LOG $1/m3-rctmux-$2-shared.log
 }
 
-run_fstrace $1 tar
-run_fstrace $1 untar
-run_fstrace $1 find
-run_fstrace $1 sqlite
+run_fstrace_alone $1 tar
+run_fstrace_shared $1 tar
+run_fstrace_alone $1 untar
+run_fstrace_shared $1 untar
+run_fstrace_alone $1 find
+run_fstrace_shared $1 find
+run_fstrace_alone $1 sqlite
+run_fstrace_shared $1 sqlite
