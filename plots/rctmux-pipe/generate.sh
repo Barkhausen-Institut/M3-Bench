fb64ad5f0ee918e44c081f7d5c20dff19cc2711d
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-10-22 07:42:21 +0200

    rctmux-pipe: determine runtime of pipeserv.

--- plots/rctmux-pipe/generate.sh
+++ plots/rctmux-pipe/generate.sh
@@ -16,6 +16,35 @@ get_ratio() {
         echo "scale=8; ($1 * 1.0) / $2" | bc
     fi
 }
+get_pipe_runtime() {
+    awk '
+    /DEBUG.*1ff11234/ {
+        p = 1
+    }
+
+    /pe04.dtu.connector: Waking up core/ {
+        if (p) {
+            match($0, /^([[:digit:]]+):/, res)
+            start = res[1]
+        }
+    }
+
+    /pe04.dtu.connector: Suspending core/ {
+        if (p) {
+            match($0, /^([[:digit:]]+):/, res)
+            time += (res[1] - start) / 1000
+        }
+    }
+
+    /DEBUG.*1ff21234/ {
+        p = 0
+        count += 1
+    }
+
+    END {
+        print(time / count)
+    }' $1
+}
 
 gen_data() {
     echo "ratio stddev"
@@ -30,6 +59,13 @@ gen_sd() {
         echo $(get_name $3-$s) $sd $ti $(get_ratio $sd $ti)
     done
 }
+gen_pipe_time() {
+    for s in 64k 128k 256k 512k 1024k; do
+        ti=$(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-alone.txt)
+        rt=$(get_pipe_runtime $1/m3-rctmux-$(get_name $2-$s)-alone.log)
+        echo $(get_name $2-$s) $rt $ti $(get_ratio $rt $ti)
+    done
+}
 
 gen_data $1 "rand-wc"       > $1/m3-rctmux-rand-wc-times.dat
 gen_data $1 "rand-sink"     > $1/m3-rctmux-rand-sink-times.dat
@@ -37,11 +73,13 @@ gen_data $1 "cat-wc"        > $1/m3-rctmux-cat-wc-times.dat
 gen_data $1 "cat-sink"      > $1/m3-rctmux-cat-sink-times.dat
 gen_data $1 "cat-wc-m3fs"   > $1/m3-rctmux-cat-wc-m3fs-times.dat
 
+echo "name runtime percent" > $1/m3-rctmux-pipe-alone-pipeserv.dat
 echo "name stddev runtime percent" > $1/m3-rctmux-pipe-alone-sd.dat
 echo "name stddev runtime percent" > $1/m3-rctmux-pipe-shared-sd.dat
 for a in rand-wc rand-sink cat-wc cat-sink cat-wc-m3fs; do
     gen_sd $1 alone $a >> $1/m3-rctmux-pipe-alone-sd.dat
     gen_sd $1 shared $a >> $1/m3-rctmux-pipe-shared-sd.dat
+    # gen_pipe_time $1 $a >> $1/m3-rctmux-pipe-alone-pipeserv.dat
 done
 
 Rscript plots/rctmux-pipe/plot.R $1/m3-rctmux-pipe.pdf \
