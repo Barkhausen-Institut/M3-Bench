ff076e1afda1c5207e847cf06c3220955e20d953
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-10-22 07:41:49 +0200

    rctmux,rctmux-pipe: determine standard deviation.

--- plots/rctmux-pipe/generate.sh
+++ plots/rctmux-pipe/generate.sh
@@ -19,11 +19,16 @@ get_ratio() {
 
 gen_data() {
     echo "ratio stddev"
-    echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$(get_name $2-64k)-alone.txt) $(get_m3_appavg $1/m3-rctmux-$(get_name $2-64k)-shared.txt)) 0
-    echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$(get_name $2-128k)-alone.txt) $(get_m3_appavg $1/m3-rctmux-$(get_name $2-128k)-shared.txt)) 0
-    echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$(get_name $2-256k)-alone.txt) $(get_m3_appavg $1/m3-rctmux-$(get_name $2-256k)-shared.txt)) 0
-    echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$(get_name $2-512k)-alone.txt) $(get_m3_appavg $1/m3-rctmux-$(get_name $2-512k)-shared.txt)) 0
-    echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$(get_name $2-1024k)-alone.txt) $(get_m3_appavg $1/m3-rctmux-$(get_name $2-1024k)-shared.txt)) 0
+    for s in 64k 128k 256k 512k 1024k; do
+        echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-alone.txt) $(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-shared.txt)) 0
+    done
+}
+gen_sd() {
+    for s in 64k 128k 256k 512k 1024k; do
+        ti=$(get_m3_appavg $1/m3-rctmux-$(get_name $3-$s)-$2.txt)
+        sd=$(get_m3_appsd $1/m3-rctmux-$(get_name $3-$s)-$2.txt)
+        echo $(get_name $3-$s) $sd $ti $(get_ratio $sd $ti)
+    done
 }
 
 gen_data $1 "rand-wc"       > $1/m3-rctmux-rand-wc-times.dat
@@ -31,6 +36,13 @@ gen_data $1 "rand-sink"     > $1/m3-rctmux-rand-sink-times.dat
 gen_data $1 "cat-wc"        > $1/m3-rctmux-cat-wc-times.dat
 gen_data $1 "cat-sink"      > $1/m3-rctmux-cat-sink-times.dat
 gen_data $1 "cat-wc-m3fs"   > $1/m3-rctmux-cat-wc-m3fs-times.dat
+
+echo "name stddev runtime percent" > $1/m3-rctmux-pipe-alone-sd.dat
+echo "name stddev runtime percent" > $1/m3-rctmux-pipe-shared-sd.dat
+for a in rand-wc rand-sink cat-wc cat-sink cat-wc-m3fs; do
+    gen_sd $1 alone $a >> $1/m3-rctmux-pipe-alone-sd.dat
+    gen_sd $1 shared $a >> $1/m3-rctmux-pipe-shared-sd.dat
+done
 
 Rscript plots/rctmux-pipe/plot.R $1/m3-rctmux-pipe.pdf \
     $1/m3-rctmux-rand-wc-times.dat \
--- plots/rctmux/generate.sh
+++ plots/rctmux/generate.sh
@@ -13,11 +13,23 @@ gen_data() {
     echo "ratio stddev"
     echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$2-alone.txt) $(get_m3_appavg $1/m3-rctmux-$2-shared.txt)) 0
 }
+gen_sd() {
+    ti=$(get_m3_appavg $1/m3-rctmux-$3-$2.txt)
+    sd=$(get_m3_appsd $1/m3-rctmux-$3-$2.txt)
+    echo $sd $ti $(get_ratio $sd $ti)
+}
 
 gen_data $1 "tar"    > $1/m3-rctmux-tar-times.dat
 gen_data $1 "untar"  > $1/m3-rctmux-untar-times.dat
 gen_data $1 "find"   > $1/m3-rctmux-find-times.dat
 gen_data $1 "sqlite" > $1/m3-rctmux-sqlite-times.dat
+
+echo "stddev runtime percent" > $1/m3-rctmux-alone-sd.dat
+echo "stddev runtime percent" > $1/m3-rctmux-shared-sd.dat
+for a in tar untar find sqlite; do
+    gen_sd $1 alone $a >> $1/m3-rctmux-alone-sd.dat
+    gen_sd $1 shared $a >> $1/m3-rctmux-shared-sd.dat
+done
 
 Rscript plots/rctmux/plot.R $1/m3-rctmux.pdf \
     $1/m3-rctmux-tar-times.dat \
