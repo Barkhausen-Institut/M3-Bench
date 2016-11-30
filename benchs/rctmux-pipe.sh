5ff7a7fd076e998af4e9f47f583f57f7438faa55
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-11-30 21:29:41 +0100

    Increased parallelism in rctmux-pipe.

--- benchs/rctmux-pipe.sh
+++ benchs/rctmux-pipe.sh
@@ -29,8 +29,11 @@ run_pipe() {
     jobs_started
 
     out=`mktemp -d`
-    M3_GEM5_OUT=$out M3_CORES=7 M3_RCTMUX_ARGS="0 $3" run $1 $2 alone
-    M3_GEM5_OUT=$out M3_CORES=6 M3_RCTMUX_ARGS="1 $3" run $1 $2 shared
+    if [ "$4" = "alone" ]; then
+        M3_GEM5_OUT=$out M3_CORES=7 M3_RCTMUX_ARGS="0 $3" run $1 $2 alone
+    else
+        M3_GEM5_OUT=$out M3_CORES=6 M3_RCTMUX_ARGS="1 $3" run $1 $2 shared
+    fi
     rm -rf $out
 }
 
@@ -38,8 +41,11 @@ run_pipe_m3fs() {
     jobs_started
 
     out=`mktemp -d`
-    M3_GEM5_OUT=$out M3_CORES=8 M3_RCTMUX_ARGS="2 $3" run $1 $2 m3fs-alone
-    M3_GEM5_OUT=$out M3_CORES=7 M3_RCTMUX_ARGS="3 $3" run $1 $2 m3fs-shared
+    if [ "$4" = "alone" ]; then
+        M3_GEM5_OUT=$out M3_CORES=8 M3_RCTMUX_ARGS="2 $3" run $1 $2 m3fs-alone
+    else
+        M3_GEM5_OUT=$out M3_CORES=7 M3_RCTMUX_ARGS="3 $3" run $1 $2 m3fs-shared
+    fi
     rm -rf $out
 }
 
@@ -47,34 +53,16 @@ jobs_init $2
 
 ./b
 
-jobs_submit run_pipe $1 rand-wc-64k         "2 /bin/rand $((64*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-128k        "2 /bin/rand $((128*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-256k        "2 /bin/rand $((256*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-512k        "2 /bin/rand $((512*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-1024k       "2 /bin/rand $((1024*1024)) /bin/wc"
+for type in alone shared; do
+    for size in 64 128 256 512 1024; do
+        jobs_submit run_pipe $1 rand-wc-${size}k        "2 /bin/rand $(($size * 1024)) /bin/wc" $type
+        jobs_submit run_pipe $1 cat-wc-${size}k         "2 /bin/cat /data/${size}k.txt /bin/wc" $type
+        jobs_submit run_pipe $1 rand-sink-${size}k      "2 /bin/rand $(($size * 1024)) /bin/sink" $type
+        jobs_submit run_pipe $1 cat-sink-${size}k       "2 /bin/cat /data/${size}k.txt /bin/sink" $type
 
-jobs_submit run_pipe $1 cat-wc-64k          "2 /bin/cat /data/64k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-128k         "2 /bin/cat /data/128k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-256k         "2 /bin/cat /data/256k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-512k         "2 /bin/cat /data/512k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-1024k        "2 /bin/cat /data/1024k.txt /bin/wc"
-
-jobs_submit run_pipe $1 rand-sink-64k       "2 /bin/rand $((64*1024)) /bin/sink"
-jobs_submit run_pipe $1 rand-sink-128k      "2 /bin/rand $((128*1024)) /bin/sink"
-jobs_submit run_pipe $1 rand-sink-256k      "2 /bin/rand $((256*1024)) /bin/sink"
-jobs_submit run_pipe $1 rand-sink-512k      "2 /bin/rand $((512*1024)) /bin/sink"
-jobs_submit run_pipe $1 rand-sink-1024k     "2 /bin/rand $((1024*1024)) /bin/sink"
-
-jobs_submit run_pipe $1 cat-sink-64k        "2 /bin/cat /data/64k.txt /bin/sink"
-jobs_submit run_pipe $1 cat-sink-128k       "2 /bin/cat /data/128k.txt /bin/sink"
-jobs_submit run_pipe $1 cat-sink-256k       "2 /bin/cat /data/256k.txt /bin/sink"
-jobs_submit run_pipe $1 cat-sink-512k       "2 /bin/cat /data/512k.txt /bin/sink"
-jobs_submit run_pipe $1 cat-sink-1024k      "2 /bin/cat /data/1024k.txt /bin/sink"
-
-jobs_submit run_pipe_m3fs $1 cat-wc-64k     "5 /bin/cat /foo/data/16k.txt /foo/data/16k.txt /foo/data/16k.txt /foo/data/16k.txt /bin/wc"
-jobs_submit run_pipe_m3fs $1 cat-wc-128k    "5 /bin/cat /foo/data/32k.txt /foo/data/32k.txt /foo/data/32k.txt /foo/data/32k.txt /bin/wc"
-jobs_submit run_pipe_m3fs $1 cat-wc-256k    "5 /bin/cat /foo/data/64k.txt /foo/data/64k.txt /foo/data/64k.txt /foo/data/64k.txt /bin/wc"
-jobs_submit run_pipe_m3fs $1 cat-wc-512k    "5 /bin/cat /foo/data/128k.txt /foo/data/128k.txt /foo/data/128k.txt /foo/data/128k.txt /bin/wc"
-jobs_submit run_pipe_m3fs $1 cat-wc-1024k   "5 /bin/cat /foo/data/256k.txt /foo/data/256k.txt /foo/data/256k.txt /foo/data/256k.txt /bin/wc"
+        f="/foo/data/$(($size / 4))k.txt"
+        jobs_submit run_pipe_m3fs $1 cat-wc-${size}k    "5 /bin/cat $f $f $f $f /bin/wc" $type
+    done
+done
 
 jobs_wait
