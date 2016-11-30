24a01e8881e715dde0c1378ad2583fab7393312b
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-11-30 16:58:08 +0100

    Removed duplications in rctmux-pipe (oops).

--- benchs/rctmux-pipe.sh
+++ benchs/rctmux-pipe.sh
@@ -59,18 +59,6 @@ jobs_submit run_pipe $1 cat-wc-256k         "2 /bin/cat /data/256k.txt /bin/wc"
 jobs_submit run_pipe $1 cat-wc-512k         "2 /bin/cat /data/512k.txt /bin/wc"
 jobs_submit run_pipe $1 cat-wc-1024k        "2 /bin/cat /data/1024k.txt /bin/wc"
 
-jobs_submit run_pipe $1 rand-wc-64k         "2 /bin/rand $((64*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-128k        "2 /bin/rand $((128*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-256k        "2 /bin/rand $((256*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-512k        "2 /bin/rand $((512*1024)) /bin/wc"
-jobs_submit run_pipe $1 rand-wc-1024k       "2 /bin/rand $((1024*1024)) /bin/wc"
-
-jobs_submit run_pipe $1 cat-wc-64k          "2 /bin/cat /data/64k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-128k         "2 /bin/cat /data/128k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-256k         "2 /bin/cat /data/256k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-512k         "2 /bin/cat /data/512k.txt /bin/wc"
-jobs_submit run_pipe $1 cat-wc-1024k        "2 /bin/cat /data/1024k.txt /bin/wc"
-
 jobs_submit run_pipe $1 rand-sink-64k       "2 /bin/rand $((64*1024)) /bin/sink"
 jobs_submit run_pipe $1 rand-sink-128k      "2 /bin/rand $((128*1024)) /bin/sink"
 jobs_submit run_pipe $1 rand-sink-256k      "2 /bin/rand $((256*1024)) /bin/sink"
