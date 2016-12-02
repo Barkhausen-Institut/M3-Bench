62f9e47cc5c54e91d503b06513b44a58c1a5b46f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-12-02 16:05:45 +0100

    More output in linux-pipe benchmark.

--- benchs/linux-pipe.sh
+++ benchs/linux-pipe.sh
@@ -22,11 +22,19 @@ cd xtensa-linux
 ./b mkbr
 
 run() {
+    /bin/echo -e "\e[1mStarting $3\e[0m"
+
     jobs_started
 
     mkdir -p $1/lx-$3
     BENCH_CMD="$2" GEM5_OUT=$1/lx-$3 GEM5_CP=1 LX_CORES=2 ./b bench
-    extract_results < $1/lx-$3/res.txt > $1/lx-$3-output.txt
+
+    if [ $? -eq 0 ]; then
+        /bin/echo -e "\e[1mFinished $3:\e[0m \e[1;32mSUCCESS\e[0m"
+        extract_results < $1/lx-$3/res.txt > $1/lx-$3-output.txt
+    else
+        /bin/echo -e "\e[1mFinished $3:\e[0m \e[1;31mFAILED\e[0m"
+    fi
 }
 
 jobs_init $2
