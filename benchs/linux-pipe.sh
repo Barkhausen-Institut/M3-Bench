7b668e6656bb9adad7ebf82128dbbeab62f6edb9
Nils Asmussen <nils@script-solution.de>
2016-12-03 08:08:37 +0100

    linux-pipe: suppress output.

--- benchs/linux-pipe.sh
+++ benchs/linux-pipe.sh
@@ -27,7 +27,7 @@ run() {
     jobs_started
 
     mkdir -p $1/lx-$3
-    BENCH_CMD="$2" GEM5_OUT=$1/lx-$3 GEM5_CP=1 LX_CORES=2 ./b bench
+    BENCH_CMD="$2" GEM5_OUT=$1/lx-$3 GEM5_CP=1 LX_CORES=2 ./b bench 1>/dev/null 2>/dev/null
 
     if [ $? -eq 0 ]; then
         /bin/echo -e "\e[1mFinished $3:\e[0m \e[1;32mSUCCESS\e[0m"
