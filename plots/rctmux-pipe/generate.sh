5fc39d276a23f164dd60ce94d0c785654d6fa965
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-12-03 08:10:24 +0100

    rctmux-pipe: less code duplication.

--- plots/rctmux-pipe/generate.sh
+++ plots/rctmux-pipe/generate.sh
@@ -50,35 +50,38 @@ get_pipe_runtime() {
     }' $1
 }
 
+sizes="64k 128k 256k 512k 1024k"
+apps="rand-wc rand-sink cat-wc cat-sink cat-wc-m3fs"
+
 gen_data() {
     echo "ratio stddev"
-    for s in 64k 128k 256k 512k 1024k; do
+    for s in $sizes; do
         echo $(get_ratio $(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-alone.txt) $(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-shared.txt)) 0
     done
 }
 gen_abs_data() {
     echo "time"
-    for s in 64k 128k 256k 512k 1024k; do
+    for s in $sizes; do
         echo $(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-alone.txt)
         echo $(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-shared.txt)
     done
 }
 gen_sd() {
-    for s in 64k 128k 256k 512k 1024k; do
+    for s in $sizes; do
         ti=$(get_m3_appavg $1/m3-rctmux-$(get_name $3-$s)-$2.txt)
         sd=$(get_m3_appsd $1/m3-rctmux-$(get_name $3-$s)-$2.txt)
         echo $(get_name $3-$s) $sd $ti $(get_ratio $sd $ti)
     done
 }
 gen_pipe_time() {
-    for s in 64k 128k 256k 512k 1024k; do
+    for s in $sizes; do
         ti=$(get_m3_appavg $1/m3-rctmux-$(get_name $2-$s)-alone.txt)
         rt=$(get_pipe_runtime $1/m3-rctmux-$(get_name $2-$s)-alone.log)
         echo $(get_name $2-$s) $rt $ti $(get_ratio $rt $ti)
     done
 }
 
-for a in rand-wc rand-sink cat-wc cat-sink cat-wc-m3fs; do
+for a in $apps; do
     gen_data $1 $a > $1/m3-rctmux-$a-times.dat
     gen_abs_data $1 $a > $1/m3-rctmux-$a-abstimes.dat
 done
@@ -86,7 +89,7 @@ done
 echo "name runtime percent" > $1/m3-rctmux-pipe-alone-pipeserv.dat
 echo "name stddev runtime percent" > $1/m3-rctmux-pipe-alone-sd.dat
 echo "name stddev runtime percent" > $1/m3-rctmux-pipe-shared-sd.dat
-for a in rand-wc rand-sink cat-wc cat-sink cat-wc-m3fs; do
+for a in $apps; do
     gen_sd $1 alone $a >> $1/m3-rctmux-pipe-alone-sd.dat
     gen_sd $1 shared $a >> $1/m3-rctmux-pipe-shared-sd.dat
     # gen_pipe_time $1 $a >> $1/m3-rctmux-pipe-alone-pipeserv.dat
