bd9de8bf8ef22189773b705aad23f0a03d0383fb
Nils Asmussen <nils@script-solution.de>
2016-12-06 14:43:48 +0100

    Improved rctmux benchmark.

--- benchs/rctmux.sh
+++ benchs/rctmux.sh
@@ -33,7 +33,6 @@ run() {
     if [ $? -eq 0 ]; then
         /bin/echo -e "\e[1mFinished $2-$3:\e[0m \e[1;32mSUCCESS\e[0m"
         ./src/tools/bench.sh $M3_GEM5_OUT/gem5.log > $1/m3-rctmux-$2-$3.txt
-        mv $M3_GEM5_OUT/gem5.log $1/m3-rctmux-$2-$3.log
     else
         /bin/echo -e "\e[1mFinished $2-$3:\e[0m \e[1;31mFAILED\e[0m"
     fi
@@ -43,16 +42,18 @@ run_fstrace() {
     # rebuild it first
     cp $1/lx-fstrace-$2-30cycles.txt-opcodes.c src/apps/fstrace/m3fs/trace.c
     ./b 1>/dev/null 2>/dev/null
+    if [ $? -ne 0 ]; then
+        /bin/echo -e "\e[1;31mBuild failed\e[0m"
+        exit
+    fi
 
-    out=`mktemp -d`
+    out=$1/m3-rctmux-$2-$3
 
     if [ "$3" = "alone" ]; then
         M3_GEM5_OUT=$out M3_CORES=6 M3_RCTMUX_ARGS="0 2 /bin/fstrace-m3fs /tmp/1/ /bin/fstrace-m3fs /tmp/2/" run $1 $2 $3
     else
         M3_GEM5_OUT=$out M3_CORES=5 M3_RCTMUX_ARGS="1 2 /bin/fstrace-m3fs /tmp/1/ /bin/fstrace-m3fs /tmp/2/" run $1 $2 $3
     fi
-
-    rm -rf $out
 }
 
 jobs_init $2
