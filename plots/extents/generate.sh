dddb5c0500133d4fa85b7203b98cbb29c99d1eae
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-08-08 11:56:36 +0200

    Updated extents benchmark.

--- plots/extents/generate.sh
+++ plots/extents/generate.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-get_values() {
-    for i in 16 32 64 128 256 512 1024 2048; do
-        val=`grep "TIME: 0001 :" $1/m3-$2-$i.txt | ./tools/m3-avg.awk`
-        echo -n $(($val / 1000)) " "
-    done
-}
+. tools/helper.sh
+
+mhz=`get_mhz $1/m3-extents-rd-16/output.txt`
 
 read_avgs=$1/m3-frag-read.dat
 write_avgs=$1/m3-frag-write.dat
 
-get_values $1 fsread | xargs > $read_avgs
-get_values $1 fswrite | xargs > $write_avgs
+echo -n > $read_avgs
+echo -n > $write_avgs
+
+bpe="16 32 64 128 256 512"
+for b in $bpe; do
+    ./m3/src/tools/bench.sh $1/m3-extents-rd-$b/gem5.log $mhz 0 | grep "TIME: 0001" | awk '{ print $4 }' >> $read_avgs
+    ./m3/src/tools/bench.sh $1/m3-extents-wr-$b/gem5.log $mhz 0 | grep "TIME: 0001" | awk '{ print $4 }' >> $write_avgs
+done
 
 Rscript plots/extents/plot.R $1/extents.pdf $read_avgs $write_avgs
--- plots/extents/plot.R
+++ plots/extents/plot.R
@@ -4,23 +4,23 @@ args <- commandArgs(trailingOnly = TRUE)
 scaling <- 1.2
 namescale <- 1.1
 
-read_times <- read.table(as.character(args[2]), header=FALSE, sep=" ")
-write_times <- read.table(as.character(args[3]), header=FALSE, sep=" ")
+read_times = scan(args[2]) / 1000
+write_times = scan(args[3]) / 1000
 
 pdf(as.character(args[1]), width=7, height=3, useDingbats=FALSE)
 par(mar=c(5.1,5.1,2.1,2.1))
 par(cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling)
 
 # Graph cars using blue points overlayed by a line
-plot(as.integer(read_times), ylim=c(0,1500), type="o", pch=0, axes=FALSE, ylab="Time (K cycles)", xlab="Blocks per extent")
-lines(as.integer(write_times), ylim=c(0,1500), type="o", pch=1, lty="dashed")
+plot(read_times, ylim=c(0,2000), type="o", pch=0, axes=FALSE, ylab="Time (K cycles)", xlab="Blocks per extent")
+lines(write_times, ylim=c(0,2000), type="o", pch=1, lty="dashed")
 
 axis(side = 1, at = 1:8, lab = c("16","32","64","128","256","512","1024","2048"))
-axis(side = 2, at = seq(0, 1500, by = 300), labels = TRUE)
+axis(side = 2, at = seq(0, 2000, by = 500), labels = TRUE)
 
 linetype <- c(1:2)
 plotchar <- seq(0, 2, 1)
-legend("topright", c("Reading", "Writing"), cex=namescale, pch=plotchar, lty=linetype)
+legend("topright", c("Reading", "Writing"), horiz=T, cex=namescale, pch=plotchar, lty=linetype)
 
 box(col = 'black')
 
