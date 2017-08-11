06c377afd78f6244efc5d3330ccb12b11d068137
Nils Asmussen <nils@os.inf.tu-dresden.de>
2017-08-11 13:21:24 +0200

    Fixed legend in pagefaults plot.

--- plots/pagefaults/pf-plot.R
+++ plots/pagefaults/pf-plot.R
@@ -29,7 +29,7 @@ plot = barplot(
     names=c("Anon 1P", "File 1P"),
     ylim = c(0, 30),
     ylab = "Duration (K Cycles)",
-    col=gray.colors(4)[-2]
+    col=gray.colors(3)
 )
 error.bar(plot,
     as.double(cbind(anon1v[-2],file1v[-2])),
@@ -44,14 +44,14 @@ plot = barplot(
     names=c("Anon 4P", "File 4P"),
     ylim = c(0, 30),
     axes=F,
-    col=gray.colors(4)[c(-1,-2)]
+    col=gray.colors(3)[-1]
 )
 error.bar(plot,
     as.double(cbind(anon4v[c(-1,-2)], file4v[c(-1,-2)])),
     as.double(cbind(anon4d[c(-1,-2)], file4d[c(-1,-2)])))
 
-legend("topright", c("Linux", "M3c-A", "M3c-C", "M3c-C*"), xpd=TRUE, horiz=F, bty="n",
-    inset=c(0,-0.06), cex=scaling, fill=gray.colors(4))
+legend("topright", c("Linux", "M3c-C", "M3c-C*"), xpd=TRUE, horiz=F, bty="n",
+    inset=c(0,-0.06), cex=scaling, fill=gray.colors(3))
 
 dev.off()
 embed_fonts(as.character(args[1]))
