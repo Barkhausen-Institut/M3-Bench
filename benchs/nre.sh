b8e20dda632d9d26bf02bc43e56c0ddc243e997e
Nils Asmussen <nils@script-solution.de>
2017-10-06 16:02:36 +0200

    Updated NRE benchmark.

--- benchs/nre.sh
+++ benchs/nre.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# use this gem5 version, because the atomic rwm operations do not work on the new version
+export GEM5_DIR=`readlink -f gem5`
+
 cd NRE/nre
 
 NRE_TARGET=x86_64 NRE_BUILD=release ./b gem5 boot/test
