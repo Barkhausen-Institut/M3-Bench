16797d00c4433269cbd163a97e14ba7cee60c337
Nils Asmussen <nils@script-solution.de>
2017-10-11 15:02:47 +0200

    Improved warmup script.

--- benchs/warmup.sh
+++ benchs/warmup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+if [ "$LX_CORES" = "" ]; then
+    echo "Please specify LX_CORES!" >&2
+    exit 1
+fi
+
+# use this gem5 version, because the atomic rwm operations do not work on the new version
+export GEM5_DIR=`readlink -f gem5`
+
 cd xtensa-linux
 
 ./b mklx
 ./b mkapps
 ./b mkbr
 
-if [ "$LX_ARCH" != "xtensa" ]; then
-    # remove old checkpoints
-    rm -rf gem5/cpt.*
-    # create checkpoint
-    GEM5_CPU=TimingSimpleCPU ./b warmup
-fi
+# remove old checkpoints
+rm -rf gem5-$LX_CORES/cpt.*
+# create checkpoint
+GEM5_CPU=TimingSimpleCPU ./b warmup
