c87cd6e96e42a64da82dc853f60bed7877e5f223
Nils Asmussen <nils@script-solution.de>
2018-05-30 06:50:01 +0200

    Added suspended VPE communication benchmark.

--- benchs/rctmux-srv.sh
+++ benchs/rctmux-srv.sh
@@ -1,47 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 
 . tools/jobs.sh
 
-cfg=`readlink -f input/rctmux-srv.cfg`
+boot_c=`readlink -f input/rctmux-srv.cfg`
+cfg_a=`readlink -f input/config-accelchain.py`
+cfg_b=`readlink -f input/config-aladdin.py`
 
 cd m3
 export M3_BUILD=release
 
-export M3_GEM5_DBG=Dtu,DtuRegWrite,DtuCmd,DtuConnector
+export M3_GEM5_DBG=DtuAccelStream,DtuAccelStreamState,DtuAccelAladdin,DtuAccelAladdinState,Dtu,DtuRegWrite,DtuCmd,DtuConnector
 export M3_GEM5_CPUFREQ=3GHz M3_GEM5_MEMFREQ=1GHz
+export M3_GEM5_MMU=1 M3_GEM5_DTUPOS=2
 # export M3_GEM5_CPU=TimingSimpleCPU
-
-export M3_GEM5_CFG=config/caches.py
 
 run() {
     jobs_started
 
-    /bin/echo -e "\e[1mStarting m3-ctx-srv-$2-$3\e[0m"
+    /bin/echo -e "\e[1mStarting m3-ctx-susp-$2-$3\e[0m"
 
-    export M3_GEM5_OUT=$1/m3-ctx-srv-$2-$3
+    export M3_GEM5_OUT=$1/m3-ctx-susp-$2-$3
     mkdir -p $M3_GEM5_OUT
 
-    if [ "$3" = "c" ]; then
-        export M3_GEM5_MMU=1 M3_GEM5_DTUPOS=2
+    if [ "$2" = "c" ]; then
+        export RCTMUX_ARGS="$4"
+        if [ "$3" = "alone" ]; then
+            export M3_CORES=7
+        elif [ "$3" = "sh-srv" ]; then
+            export M3_CORES=6
+        else
+            export M3_CORES=5
+        fi
+        export M3_GEM5_CFG=config/caches.py
+        boot=$boot_c
     else
-        export M3_GEM5_MMU=0
+        if [ "$3" = "alone" ]; then
+            export M3_CORES=4 ACCEL_NUM=2
+        else
+            export M3_CORES=4 ACCEL_NUM=1
+        fi
+        if [ "$2" = "a" ]; then
+            export M3_GEM5_CFG=$cfg_a
+            boot=boot/rctmux-streamtest.cfg
+        else
+            export M3_GEM5_CFG=$cfg_b
+            boot=boot/rctmux-aladdin.cfg
+        fi
     fi
 
-    export RCTMUX_ARGS="$4"
-    if [ "$2" = "alone" ]; then
-        export M3_CORES=7
-    elif [ "$2" = "sh-srv" ]; then
-        export M3_CORES=6
-    else
-        export M3_CORES=5
-    fi
-
-    ./b run $cfg -n 1>$M3_GEM5_OUT/output.txt 2>&1
+    ./b run $boot -n 1>$M3_GEM5_OUT/output.txt 2>&1
 
     if [ "`grep 'Time: ' $M3_GEM5_OUT/output.txt`" != "" ]; then
-        /bin/echo -e "\e[1mFinished m3-ctx-srv-$2-$3:\e[0m \e[1;32mSUCCESS\e[0m"
+        /bin/echo -e "\e[1mFinished m3-ctx-susp-$2-$3:\e[0m \e[1;32mSUCCESS\e[0m"
     else
-        /bin/echo -e "\e[1mFinished m3-ctx-srv-$2-$3:\e[0m \e[1;31mFAILED\e[0m"
+        /bin/echo -e "\e[1mFinished m3-ctx-susp-$2-$3:\e[0m \e[1;31mFAILED\e[0m"
     fi
 }
 
@@ -49,10 +61,14 @@ jobs_init $2
 
 ./b
 
-for t in b c; do
-    jobs_submit run $1 alone $t "0"
-    jobs_submit run $1 sh-srv $t "1"
-    jobs_submit run $1 sh-all $t "2"
-done
+jobs_submit run $1 a alone
+jobs_submit run $1 a shared
+
+jobs_submit run $1 b alone
+jobs_submit run $1 b shared
+
+jobs_submit run $1 c alone "0"
+jobs_submit run $1 c sh-srv "1"
+jobs_submit run $1 c sh-all "2"
 
 jobs_wait
---
+++ plots/diss-ctx-susp/generate.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+. tools/helper.sh
+
+get_app_idle() {
+    awk -v type=$2 '
+        START {
+            c = 0
+        }
+
+        /DEBUG.*1ff11234/ {
+            match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+            start = m[1]
+            comstart = start
+            /*print("start:", m[1])*/
+            p = 1
+        }
+        /DEBUG.*1ff21234/ {
+            match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+            /*print("end:", m[1])*/
+            totals[c] = m[1] - start
+            c += 1
+            p = 0
+        }
+
+        /DEBUG.*1ff1cccc/ {
+            if(p) {
+                match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+                wakes[c] += m[1] - comstart
+                swstart = m[1]
+                /*print("sw-start:", m[1])*/
+            }
+        }
+        /DEBUG.*1ff2cccc/ {
+            if(p) {
+                match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+                switches[c] += m[1] - swstart
+                swend = m[1]
+                /*print("sw-end:", m[1])*/
+            }
+        }
+
+        /pe00.*src: pe=(3|4).*on behalf/ {
+            if(p && type != "alone") {
+                match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+                fwds[c] += m[1] - swend
+                astart = m[1]
+                /*print(">accel:", m[1])*/
+            }
+        }
+        /pe0(4|5).*\[rv <- 3/ {
+            if(p && type == "alone") {
+                match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+                astart = m[1]
+                /*print(">accel:", m[1])*/
+            }
+        }
+        /pe04.*\[sd -> (5|6)\]/ {
+            if(p && type == "alone") {
+                match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+                astart = m[1]
+                /*print(">accel:", m[1])*/
+            }
+        }
+
+        /pe0(4|5|6).*\[(rp|sd) -> (3|4)]/ {
+            if(p) {
+                match($1, /^[[:space:]]*([[:digit:]]+):/, m)
+                if(type == "sh-all")
+                    comstart = m[1]
+                accels[c] = m[1] - astart
+                /*print("<accel:", m[1])*/
+            }
+        }
+
+        END {
+            len = 0
+            for(i in totals) len++
+
+            wakeup_sum = 0
+            switch_sum = 0
+            fwds_sum = 0
+            rem_sum = 0
+            warmup = 8
+            for(i = warmup; i < len; i++) {
+                # print("totals", i, totals[i] / 1000)
+                # print("wakes", i, wakes[i] / 1000)
+                # print("switches", i, switches[i] / 1000)
+                # print("fwds", i, fwds[i] / 1000)
+                # print("accels", i, accels[i] / 1000)
+
+                wakeup_sum  += wakes[i] / 1000
+                switch_sum += switches[i] / 1000
+                fwds_sum += fwds[i] / 1000
+                rem_sum  += (totals[i] - (wakes[i] + switches[i] + fwds[i])) / 1000
+            }
+            print(wakeup_sum / (len - warmup),
+                  switch_sum / (len - warmup),
+                  fwds_sum / (len - warmup),
+                  rem_sum / (len - warmup))
+        }
+    ' < $1
+}
+
+nova_total() {
+    grep -P '! PingpongXPd\.cc.* \d+ cycles' $1/nre/gem5.log | \
+        sed -Ee 's/.* ([[:digit:]]+) cycles.*/\1/'
+}
+
+echo -n > $1/ctx-susp-times.dat
+
+for v in sh-all sh-srv alone; do
+    get_app_idle $1/m3-ctx-susp7-c-$v/gem5.log $v >> $1/ctx-susp-times.dat
+done
+for t in b a; do
+    for v in shared alone; do
+        get_app_idle $1/m3-ctx-susp-$t-$v/gem5.log $v >> $1/ctx-susp-times.dat
+    done
+done
+
+novarem=`nova_total $1 | head -n 1`
+echo 0 0 0 $((novarem / 3)) >> $1/ctx-susp-times.dat
+novaloc=`nova_total $1 | tail -n 1`
+echo 0 0 0 $((novaloc / 3)) >> $1/ctx-susp-times.dat
+
+Rscript plots/diss-ctx-susp/plot.R $1/eval-ctx-susp.pdf $1/ctx-susp-times.dat
