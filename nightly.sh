6bea561a9f403a323ac11fc6296ed98587513031
Nils Asmussen <nils@os.inf.tu-dresden.de>
2019-08-01 14:50:01 +0200

    nightly: added garbage collection.

--- nightly.sh
+++ nightly.sh
@@ -19,11 +19,36 @@ echo -e "\033[1mUpdating repositories...\033[0m"
 if [ $? -ne 0 ]; then exit 1; fi
 ( cd m3 && git rev-parse HEAD ) > $out/git-commit
 
+# don't re-run the tests with the same commit
 for dir in results/tests-*; do
     if [ "$dir" != "$out" ] && cmp $out/git-commit $dir/git-commit &>/dev/null; then
         echo "Current commit $(cat $dir/git-commit) has been already tested in $dir. Exiting." | tee -a $out/nightly.log
         exit 1
     fi
+done
+
+echo -e "\033[1mStarting garbage collection...\033[0m"
+# only keep the last 7 days in full size; reduce the older ones to the minimum
+num_days=7
+total=$(find results -maxdepth 1 -name "tests-*" -type d | wc -l)
+for dir in results/tests-*; do
+    if [ $total -le $num_days ]; then
+        break
+    fi
+
+    echo "$dir: removing unnecessary files and compressing into $dir.tar.xz..."
+    # remove everything but the output.txt
+    for sub in $dir/*; do
+        if [ -d $sub ]; then
+            find $sub -mindepth 1 -not -name output.txt -print0 | xargs -0 rm -f
+        fi
+    done
+
+    # pack the output.txt's into an archive
+    tar cf - $dir | xz > $dir.tar.xz
+    rm -rf $dir
+
+    total=$((total - 1))
 done
 
 echo -e "\033[1mBuilding gem5...\033[0m"
