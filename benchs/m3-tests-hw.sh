fc5801ca23ac5677cced33d7ccf71ae918ba37ea
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-08-02 10:00:19 +0200

    Updated hw benchmarks.

--- benchs/m3-tests-hw.sh
+++ benchs/m3-tests-hw.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
-bootscale=`readlink -f input/bench-scale-pipe.cfg`
-bootfstrace=`readlink -f input/fstrace.cfg`
+inputdir=`readlink -f input`
 
 cd m3
 
 export M3_TARGET=hw M3_ISA=riscv
 export M3_HW_SSH=bitest M3_HW_FPGA=1
+export M3_HW_VM=1 M3_HW_RESET=1
 
 run_bench() {
-    bpe=$5
-    dirname=m3-tests-$2-$3-$4-$5
+    bpe=$6
+    dirname=m3-tests-$2-$3-$4-$5-$6
     export M3_OUT=$1/$dirname
     mkdir -p $M3_OUT
+
+    bootprefix=""
+    if [ "$4" = "sh" ]; then
+        bootprefix="shared/"
+    fi
 
     bench=$2
 
     if [ "$bench" = "unittests" ] || [ "$bench" = "rust-unittests" ]; then
         export M3_FS=default-$bpe.img
-        cp boot/$bench.xml $M3_OUT/boot.gen.xml
+        cp boot/${bootprefix}$bench.xml $M3_OUT/boot.gen.xml
     elif [ "$bench" = "standalone" ] || [ "$bench" = "memtest" ] || [ "$bench" = "standalone-sndrcv" ]; then
+        export M3_HW_VM=0
         cp boot/kachel/$bench.xml $M3_OUT/boot.gen.xml
     elif [ "$bench" = "hello" ]; then
-        cp boot/$bench.xml $M3_OUT/boot.gen.xml
+        cp boot/${bootprefix}$bench.xml $M3_OUT/boot.gen.xml
     else
         export M3_FS=bench-$bpe.img
 
         if [[ "$bench" =~ "bench" ]]; then
-            cp boot/$bench.xml $M3_OUT/boot.gen.xml
+            cp boot/${bootprefix}$bench.xml $M3_OUT/boot.gen.xml
         elif [[ "$bench" =~ "_" ]]; then
             IFS='_' read -ra parts <<< "$bench"
             writer=${parts[0]}_${parts[1]}_${parts[0]}
             reader=${parts[0]}_${parts[1]}_${parts[1]}
             export M3_ACCEL_COUNT=0 M3_ARGS="-i 1 -r 2 -w 1 $writer $reader"
-            $bootscale > $M3_OUT/boot.gen.xml
+            $inputdir/${bootprefix}bench-scale-pipe.cfg > $M3_OUT/boot.gen.xml
         else
             export M3_ARGS="-n 4 -t -u 1 $bench"
-            $bootfstrace > $M3_OUT/boot.gen.xml
+            $inputdir/${bootprefix}fstrace.cfg > $M3_OUT/boot.gen.xml
         fi
     fi
 
@@ -78,20 +84,17 @@ fi
 
 for bpe in 32 64; do
     for build in debug release; do
-        # the *-64 runs are for performance, so don't run in debug mode
-        if [ $bpe -eq 64 ] && [ "$build" = "debug" ]; then
-            continue;
-        fi
-
-        export M3_BUILD=$build
-
-        for test in $benchs; do
-            # skip rust-unittests in debug mode, because the code gets to large
-            if [ "$build" = "debug" ] && [ "$test" = "rust-unittests" ]; then
+        for ty in ex sh; do
+            # the *-64 runs are for performance, so don't run in debug mode
+            if [ $bpe -eq 64 ] && [ "$build" = "debug" ]; then
                 continue;
             fi
 
-            run_bench $1 $test hw-$build $M3_ISA $bpe
+            export M3_BUILD=$build
+
+            for test in $benchs; do
+                run_bench $1 $test hw-$build $ty $M3_ISA $bpe
+            done
         done
     done
 done
