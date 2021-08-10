36a175306e51bf24b9ce35e8f98c2b02f038da07
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-08-10 21:21:40 +0200

    Updated microbenchmarks plot.

--- plots/asplos22-micro/plot.R
+++ plots/asplos22-micro/plot.R
@@ -2,33 +2,36 @@ library(extrafont)
 library(RColorBrewer)
 source("tools/helper.R")
 
-scaling <- .9
+scaling <- 2
 colors <- brewer.pal(n = 4, name = "Pastel1")
 
 args <- commandArgs(trailingOnly = TRUE)
 
-pdf(as.character(args[1]), width=5, height=4.5)
+pdf(as.character(args[1]), width=10, height=3)
 par(cex.lab=scaling, cex.axis=scaling, cex.main=scaling, cex.sub=scaling)
 
-par(mar=c(2.5, 4.5, 1, 0))
+par(mar=c(4.5, 10, 1, 2))
 
-vals = scan(args[2]) / 1000
-dev = scan(args[3]) / 1000
+vals = rev(scan(args[2]) / 1000)
+dev = rev(scan(args[3]) / 1000)
 zeros = rep(c(NA), 4)
 
-barplot(zeros, ylim=c(0, 10), axes=F, space=rep(0.1, 4),
-    names.arg=rep("", 4), ylab="Duration (K Cycles)")
-abline(h=c(seq(0, 10, 2)), col="gray80", lwd=2)
+barplot(zeros, xlim=c(0, 10), axes=F, space=rep(0.1, 4),
+    horiz=T, names.arg=rep("", 4), las=1, xlab="Duration (K Cycles)")
+abline(v=c(seq(0, 10, 2)), col="gray80", lwd=2)
 
 plot = barplot(
     vals,
     add=T,
-    names=c("UCS² Rem", "UCS² Loc", "Lx Syscall", "Lx Yield"),
+    horiz=T,
+    names=rev(c("UCS² Rem", "UCS² Loc", "Lx Syscall", "Lx Yield")),
     space=rep(0.1, 4),
-    ylim=c(0, 10),
-    col=colors
+    xlim=c(0, 10),
+    mgp=c(8, 1.5, 0),
+    las=1,
+    col=rev(colors)
 )
-error.bar(plot, vals, dev)
+error.bar(plot, vals, dev, horizontal=T)
 
 dev.off()
 embed_fonts(as.character(args[1]))
