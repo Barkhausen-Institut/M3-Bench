4c2abfc4c84aa0b9150f55157f16f79fce48a3bb
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-07-25 11:20:38 +0200

    Added IPC benchmarks on M³ (gem5/hw).

---
+++ benchs/rtas23-m3-ipc-gem5.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+. tools/jobs.sh
+
+cd m3 || exit 1
+
+export M3_BUILD=release
+export M3_TARGET=gem5 M3_ISA=x86_64
+if [ -z "$M3_GEM5_DBG" ]; then
+    export M3_GEM5_DBG=Tcu,TcuRegWrite,TcuCmd,TcuConnector
+fi
+export M3_GEM5_CPU=DerivO3CPU
+export M3_GEM5_CPUFREQ=3GHz M3_GEM5_MEMFREQ=1GHz
+export M3_CORES=12
+export M3_GEM5_CFG=config/caches.py
+
+./b || exit 1
+
+run_bench() {
+    type=$2
+    dirname=m3-ipc-gem5-$type
+    export M3_OUT=$1/$dirname
+    mkdir -p "$M3_OUT"
+
+    /bin/echo -e "\e[1mStarting $dirname\e[0m"
+    jobs_started
+
+    ./b run "boot/bench-pingpong-$type.xml" -n < /dev/null &> "$M3_OUT/output.txt"
+
+    if [ $? -eq 0 ]; then
+        /bin/echo -e "\e[1mFinished $dirname:\e[0m \e[1;32mSUCCESS\e[0m"
+    else
+        /bin/echo -e "\e[1mFinished $dirname:\e[0m \e[1;31mFAILED\e[0m"
+    fi
+}
+
+jobs_init "$2"
+
+for t in remote local multi; do
+    jobs_submit run_bench "$1" "$t"
+done
+
+jobs_wait
---
+++ benchs/rtas23-m3-ipc-hw.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source tools/helper.sh
+
+cd m3 || exit 1
+
+export M3_BUILD=release
+export M3_TARGET=hw M3_ISA=riscv
+export M3_HW_SSH=bitest M3_HW_FPGA=1
+export M3_HW_VM=1 M3_HW_RESET=1
+
+./b || exit 1
+
+run_bench() {
+    type=$2
+    dirname=m3-ipc-hw-$type
+    export M3_OUT=$1/$dirname
+    mkdir -p "$M3_OUT"
+
+    while true; do
+        /bin/echo -e "\e[1mStarting $dirname\e[0m"
+
+        ./b run "boot/bench-pingpong-$type.xml" -n 2>&1 | tee "$M3_OUT/output.txt"
+
+        sed --in-place -e 's/\x1b\[0m//g' "$M3_OUT/output.txt"
+
+        if bench_succeeded "$dirname" "$M3_OUT/output.txt" 'pingpong'; then
+            break
+        fi
+    done
+}
+
+for t in remote local multi; do
+    run_bench "$1" "$t"
+done
