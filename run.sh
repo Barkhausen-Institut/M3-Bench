d3f630d409125d937325fc3c8382730a3e451c14
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-08-05 15:23:35 +0200

    run.sh: removed obsolete defaults/requirements.

--- run.sh
+++ run.sh
@@ -13,22 +13,6 @@ if [ $# -ne 5 ]; then
     exit 1
 fi
 
-if [ "$M3_TARGET" = "" ]; then
-    echo "Please define M3_TARGET first." 1>&2
-    exit 1
-fi
-
-case $M3_TARGET in
-    t3)
-        export M3_LOG=run/xtsc.log
-        export LX_ARCH=xtensa LX_PLATFORM=xtensa LX_BUILD=release
-        ;;
-    gem5)
-        export M3_LOG=$M3_GEM5_OUT/gem5.log
-        export LX_ARCH=x86_64 LX_PLATFORM=gem5 LX_BUILD=release
-        ;;
-esac
-
 name=$1
 benchs=$2
 posts=$3
