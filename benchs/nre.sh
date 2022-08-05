48d44e310f216ab134a744d2978cdea216819ec8
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-08-05 15:24:04 +0200

    Added NRE pingpong benchmark.

--- benchs/nre.sh
+++ benchs/nre.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
 # use this gem5 version, because the atomic rwm operations do not work on the new version
-export GEM5_DIR=`readlink -f gem5-lx`
+export GEM5_DIR=$(readlink -f gem5-official)
+export GEM5_OUT="$1"
 
 cd NRE/nre
 
 NRE_TARGET=x86_64 NRE_BUILD=release ./b gem5 boot/test
-
-if [ $? -eq 0 ]; then
-    mkdir -p $1/nre
-    cp m5out/system.pc.com_1.terminal $1/nre/gem5.log
-fi
