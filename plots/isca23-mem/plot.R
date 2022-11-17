9072a2bcfb88586f6e30067642a2ad011d07cd4b
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-11-17 18:43:09 +0100

    Memory: added standard deviation.

--- plots/isca23-mem/plot.R
+++ plots/isca23-mem/plot.R
@@ -3,19 +3,33 @@ library(RColorBrewer)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-ipc <- read.table(header = TRUE, file = as.character(args[2]))
-ipc$datasize[ipc$datasize == "4096b"] <- "4KiB"
-ipc$datasize[ipc$datasize == "65536b"] <- "64KiB"
-ipc$datasize[ipc$datasize == "1048576b"] <- "1MiB"
-ipc$datasize[ipc$datasize == "16777216b"] <- "16MiB"
-ipc$datasize[ipc$datasize == "268435456b"] <- "256MiB"
+raw <- read.table(header = TRUE, file = as.character(args[2]))
+raw$datasize[raw$datasize == "4096b"] <- "4K"
+raw$datasize[raw$datasize == "65536b"] <- "64K"
+raw$datasize[raw$datasize == "1048576b"] <- "1M"
+raw$datasize[raw$datasize == "16777216b"] <- "16M"
+raw$datasize[raw$datasize == "268435456b"] <- "256M"
+
+data <- filter(raw, platform != "SR-IOV+IOMMUs")
+sriov <- filter(raw, platform == "SR-IOV+IOMMUs")
 
 colors <- brewer.pal(n = 4, name = "Pastel1")
-columns <- c("1b", "16b", "256b", "4KiB", "64KiB", "1MiB", "16MiB", "256MiB")
+columns <- c("1b", "16b", "256b", "4K", "64K", "1M", "16M", "256M")
 
-ggplot(data=ipc, mapping=aes(x=datasize, y=latency, fill=platform)) +
+for(v in columns) {
+    sub <- filter(sriov, datasize == v)
+    avg <- round(mean(sub$latency))
+    stddev <- round(sd(sub$latency))
+    data[nrow(data) + 1, 1:2] = c("SR-IOV+IOMMUs", v)
+    data[nrow(data), 3:4] = c(avg, stddev)
+}
+
+print(data)
+
+ggplot(data=data, mapping=aes(x=datasize, y=latency, fill=platform)) +
   scale_x_discrete(limits=columns) +
   geom_bar(stat="summary", position=position_dodge(), colour="black", size=.1) +
+  geom_errorbar(aes(ymin=latency-sd, ymax=latency+sd), width=.4, position=position_dodge(.9)) +
   scale_y_log10() +
   scale_fill_manual(values=colors) +
   labs(x="Data size (bytes)",y="Latency (cycles)") +
--- plots/isca23-mem/post.sh
+++ plots/isca23-mem/post.sh
@@ -5,8 +5,8 @@
 extract_m3() {
     sed -e 's/\x1b\[0m//g' "$1/m3-mem-$2/log.txt" | awk -e '
         /PERF/ {
-            if (match($0, /PERF "write ([0-9]+)b with [0-9]+b buf": ([0-9]+) cycles/, m) != 0) {
-                printf("M³-'"$2"' %db %d\n", m[1], m[2])
+            if (match($0, /PERF "write ([0-9]+)b with [0-9]+b buf": ([0-9]+) cycles.* ([0-9]+) cycles with/, m) != 0) {
+                printf("M³-'"$2"' %db %d %d\n", m[1], m[2], m[3])
             }
         }
     '
@@ -15,8 +15,8 @@ extract_m3() {
 extract_shm() {
     awk -e '
         /[0-9]+b: / {
-            if (match($0, /([0-9]+)b: ([0-9]+) cycles per run/, m) != 0) {
-                printf("MMU+IPIs %db %d\n", m[1], m[2])
+            if (match($0, /([0-9]+)b: ([0-9]+) cycles per run; sd=([0-9]+)/, m) != 0) {
+                printf("IPIs+MMU %db %d %d\n", m[1], m[2], m[3])
             }
         }
     ' "$1/shm-mem/log.txt"
@@ -27,13 +27,14 @@ extract_sriov() {
         for i in {1..8}; do
             time=$(echo "$line" | cut -d ',' -f "$i")
             if [ $time -ne 0 ]; then
-                echo "SR-IOV+IOMMUs $((1 << ((i - 1) * 4)))b $time"
+                size=$((1 << ((i - 1) * 4)))
+                echo "SR-IOV+IOMMUs ${size}b $time 0"
             fi
         done
     done
 }
 
-echo "platform datasize latency" > "$1/mem.dat"
+echo "platform datasize latency sd" > "$1/mem.dat"
 extract_m3 "$1" "memcpy" >> "$1/mem.dat"
 extract_m3 "$1" "tcu" >> "$1/mem.dat"
 extract_shm "$1" >> "$1/mem.dat"
