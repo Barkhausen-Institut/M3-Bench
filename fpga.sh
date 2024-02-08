1c9593dc84ad91ae1e408b1370ea8391686ddb60
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-02-08 11:50:59 +0100

    Use local Xilinx installation.

--- fpga.sh
+++ fpga.sh
@@ -3,5 +3,5 @@ export LX_HW_SSH=localhost
 export M3_HW_FPGA_HOST=localhost
 export M3_HW_FPGA_DIR=m3
 export M3_HW_FPGA_NO=1
-export M3_HW_VIVADO=/home/hrniels/Applications/Xilinx/Vivado_Lab/2019.1/bin/vivado_lab
+export M3_HW_VIVADO=/home/rtas24-ae/Xilinx/Vivado_Lab/2019.1/bin/vivado_lab
 export M3_HW_TTY=/dev/ttyUSB2
