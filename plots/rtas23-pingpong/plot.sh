22c68d242f0e3fd8a6d340a8a245b8d1779ac478
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-02-09 10:38:30 +0100

    pingpong: write table into file.

--- plots/rtas23-pingpong/plot.sh
+++ plots/rtas23-pingpong/plot.sh
@@ -2,4 +2,5 @@
 
 . tools/helper.sh
 
-rscript_crop plots/rtas23-pingpong/plot.R "$1/eval-pingpong.pdf" "$1/pingpong.dat"
+rscript_crop plots/rtas23-pingpong/plot.R "$1/eval-pingpong.pdf" "$1/pingpong.dat" | tee "$1/pingpong.table"
+sed --in-place -e '/.*textbf/!d' "$1/pingpong.table"
