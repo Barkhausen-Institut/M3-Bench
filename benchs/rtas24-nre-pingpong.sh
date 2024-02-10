f5f64fdb231c7057d5e9e038aba7c7fe3479688e
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-02-10 17:32:33 +0100

    nre-pingpong: silence gem5 output and detect errors.

--- benchs/rtas24-nre-pingpong.sh
+++ benchs/rtas24-nre-pingpong.sh
@@ -1,16 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 cd NRE/nre
 
-export GEM5=1 GEM5_OUT="$1/nre-ipc-gem5"
+dirname="nre-ipc-gem5"
+export GEM5=1 GEM5_OUT="$1/$dirname"
 export NRE_TARGET=x86_64 NRE_BUILD=release
 
 mkdir -p "$GEM5_OUT"
 
 # rebuild nova to ensure that the GEM5 define is considered
 ( cd ../kernel/nova/build && ARCH=$NRE_TARGET make clean )
+./b
 
-./b gem5 boot/test
+/bin/echo -e "\e[1mStarting $dirname\e[0m"
 
+./b gem5 boot/test -n 2> "$GEM5_OUT/logerr.txt"
+
+if [ "$(grep 'Total failures: 0' "$GEM5_OUT/system.pc.com_1.device")" != "" ]; then
+    /bin/echo -e "\e[1mFinished $dirname:\e[0m \e[1;32mSUCCESS\e[0m"
+else
+    /bin/echo -e "\e[1mFinished $dirname:\e[0m \e[1;31mFAILED\e[0m"
+fi
